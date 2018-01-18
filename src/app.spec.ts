3fb4fdc467d876c69a8739cfbce6978cba15012a
Sean Sobey SAHL <seans@sahomeloans.com>
2018-01-18 15:17:07 +0200

    Implemented mocha sidebar.

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -4,4 +4,10 @@ describe('example', () => {
 	it('passes', () => {
 		assert.equal(true, true);
 	});
+	it('passes again', () => {
+		assert.equal(true, true);
+	});
+	it('passes again again', () => {
+		assert.equal(true, true);
+	});
 });
