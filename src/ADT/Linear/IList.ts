e39c7018310a4b7147a458f1be3381334a7ff80f
Sean Sobey SAHL <seans@sahomeloans.com>
2018-01-29 11:54:31 +0200

    Added some interface docs.

--- src/ADT/Linear/IList.ts
+++ src/ADT/Linear/IList.ts
@@ -4,29 +4,29 @@ import { ICollection } from '../ICollection';
 export interface IList<T> extends ICollection<T> {
 	/**
 	 * Add an element to the start of the list.
-	 * 
-	 * @param {T} item 
+	 *
+	 * @param {T} item The item.
 	 * @memberof IList
 	 */
 	prepend(item: T): void;
 	/**
 	 * Add an element to the end of the list.
-	 * 
-	 * @param {T} item 
+	 *
+	 * @param {T} item The item.
 	 * @memberof IList
 	 */
 	append(item: T): void;
 	/**
 	 * Get the element at the start of the list, or throw exception if the list is empty.
-	 * 
-	 * @returns {T} 
+	 *
+	 * @returns {T}
 	 * @memberof IList
 	 */
 	head(): T;
 	/**
 	 * Get all the element in the list except the first, or throw exception if the list is empty.
-	 * 
-	 * @returns {IList<T>} 
+	 *
+	 * @returns {IList<T>}
 	 * @memberof IList
 	 */
 	tail(): IList<T>;
